e579bb3369437aefcdc57790099ffe36f06e2369
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-11-21 20:27:10 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './pages/ProjectsDetails/ProjectsDetails'
+import ProjectsDetails from './pages/ProjectsDetails/ProjectsDetails';
+
+const App=()=>{
+  return (
+    <>
+    <ProjectsDetails/>
+    </>
+  )
+}
+
+export default App;
